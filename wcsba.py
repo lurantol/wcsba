954158f40bf68131c8bc8916099285f1082a01ae
Yuri Lubomirski <lurantol@gmail.com>
2024-05-21 21:05:01 +0300

    change window title

--- wcsba.py
+++ wcsba.py
@@ -6,7 +6,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Пример интерфейса")
+        self.setWindowTitle("West Coast Swing Broadcast App)
         self.setGeometry(100, 100, 400, 300)
 
         # Создаем виджет вкладок
